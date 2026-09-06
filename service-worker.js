e7db28292feaf704c56966c5d4787c6ca777d6c0
Gerard <36046404+CookieCloud@users.noreply.github.com>
2026-09-06 16:53:18 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,9 @@
 const CACHE_NAME = 'puzzle-v1';
-const urlsToCache = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/script.js',
-    '/manifest.json'
-];
 
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(urlsToCache).catch(() => {
-                console.log('Alguns recursos no es van poder cachear');
-            });
-        })
-    );
+self.addEventListener('install', () => {
+    self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        }).catch(() => {
-            return caches.match('/index.html');
-        })
-    );
+self.addEventListener('activate', () => {
+    self.clients.claim();
 });
