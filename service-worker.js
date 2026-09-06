5f0fd0d5db0f96035278eaa1b1b2bcbc813d08bd
Gerard <36046404+CookieCloud@users.noreply.github.com>
2026-09-06 16:57:40 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,9 +0,0 @@
-const CACHE_NAME = 'puzzle-v1';
-
-self.addEventListener('install', () => {
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', () => {
-    self.clients.claim();
-});
